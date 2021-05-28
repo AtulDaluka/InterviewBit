30126a9392d101522eba4ebd9ca21e8fe616e2a6
AtulDaluka <atuldaluka19@gmail.com>
2021-05-28 14:44:14 +0530

    Update Greedy_solution.py

--- Dynamic Programming/Longest Common Subsequence/Greedy_solution.py
+++ Dynamic Programming/Longest Common Subsequence/Greedy_solution.py
@@ -2,11 +2,15 @@ class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         pile_top = [1000000 for each in range(len(nums))]
         pile_length = [0 for each in range(len(nums))]
-        for i in reversed(range(len(nums))):
+        for i in (range(len(nums))):
             for j in range(len(pile_top)):
-                if(pile_top[j]>nums[i]):
+                if(pile_top[j]>=nums[i]):
                     pile_length[j] += 1
                     pile_top[j] = nums[i]
-                    print(pile_top,pile_length)
+                    # print(pile_top,pile_length)
                     break
-        return max(pile_length)
+        count = 0
+        for j in range(len(pile_length)):
+            if(pile_length[j]!=0):
+                count += 1
+        return count
